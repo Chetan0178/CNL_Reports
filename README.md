f61bac4824914e6c9b34f00ed61c2cd6802a3628
Chetan0178 <chetan.chaudhari@qodefarm.com>
2024-09-23 19:23:07 +0530

    able to call API after clicking on chart button and able to show chart with static data

--- README.md
+++
@@ -1,2 +0,0 @@
-# CNL_Reports
-This repo is meant for creating reporting tools using diff-diff charts 
--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { MychartComponent } from './mychart/mychart.component';
-
-export const routes: Routes = [
-    {
-        path: 'chart',component:MychartComponent
-    }
-];
