84b06994ed88edad6b761d51aacd47f984b0b468
Chetan0178 <chetan.chaudhari@qodefarm.com>
2024-09-26 15:38:47 +0530

    added Home button to navigate Home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'report-without-standalone';
+
+  constructor(private router: Router) {}
+
+
+  isOnChartPage(): boolean {
+    return this.router.url === '/chart'; // Check if the current URL is '/chart'
+  }
 }
