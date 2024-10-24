d556a6000a1246c2e28fdf2fde01aea0c84a2a86
Chetan0178 <chetan.chaudhari@qodefarm.com>
2024-10-24 20:17:39 +0530

    now we can see selected table's columns in well orginized way

--- src/app/query-builder/query-builder.component.ts
+++ src/app/query-builder/query-builder.component.ts
@@ -1,22 +1,25 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, Renderer2 } from '@angular/core';
+import { Component } from '@angular/core';
+import { API_HOST } from '../../assets/api.config';
+
 
 @Component({
   selector: 'app-query-builder',
   templateUrl: './query-builder.component.html',
-  styleUrl: './query-builder.component.css'
+  styleUrls: ['./query-builder.component.css'] // Corrected styleUrls here
 })
 export class QueryBuilderComponent {
-  tables: string[] = []; // Holds the list of tables
-  isLoading = false; // Indicates if the API call is in progress
+  tables: string[] = [];
+  selectedColumns: { [tableName: string]: string[] } = {};
+  isLoading = false;
 
   constructor(private http: HttpClient) {}
 
-  // Function to load tables from API
   loadTables() {
     this.isLoading = true;
-    this.http.get<any>('http://127.0.0.1:8000/api/tables/').subscribe(
-      (response) => {
+    const apiUrl = `${API_HOST}/api/tables/`;
+    this.http.get(apiUrl).subscribe(
+      (response:any) => {
         this.tables = response.tables;
         this.isLoading = false;
       },
@@ -25,5 +28,31 @@ export class QueryBuilderComponent {
         this.isLoading = false;
       }
     );
+  }
+
+  loadColumns(tableName: string, event: any) {
+    const isChecked = event.target.checked;
+
+    if (isChecked) {
+      this.isLoading = true;
+      const columns_api_url = `${API_HOST}/api/tables/${tableName}/columns/`;
+      this.http.get(columns_api_url).subscribe(
+        (response:any) => {
+          this.selectedColumns[tableName] = response.columns;
+          this.isLoading = false;
+        },
+        (error) => {
+          console.error('Error fetching columns for table:', tableName, error);
+          this.isLoading = false;
+        }
+      );
+    } else {
+      delete this.selectedColumns[tableName];
+    }
+  }
+
+  // Method to return the keys of the selectedColumns object
+  getSelectedColumnKeys(): string[] {
+    return Object.keys(this.selectedColumns);
   }
 }
